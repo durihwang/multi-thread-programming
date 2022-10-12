7da39bc9c71431b3aa25a5e8a060b1d0c304400b
황두리 <hdr@gabia.com>
2022-10-12 18:01:41 +0900

    first commit

---
+++ src/main/java/com/study/multithreadprogramming/MultiThreadProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.study.multithreadprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/multithreadprogramming/MultiThreadProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.multithreadprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
